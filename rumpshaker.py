3c058ad823e751897123bce90880de997cd1eade
Justin Donaldson <justin@hushh.ai>
2024-07-22 13:42:54 -0700

    rumpshaker custom text

--- rumpshaker.py
+++ rumpshaker.py
@@ -1,8 +1,13 @@
 import rumps
+import argparse
 
-class HelloWorldApp(rumps.App):
-    def __init__(self):
-        super(HelloWorldApp, self).__init__("Hello World")
+class CustomTextApp(rumps.App):
+    def __init__(self, text):
+        super(CustomTextApp, self).__init__(text)
 
 if __name__ == "__main__":
-    HelloWorldApp().run()
+    parser = argparse.ArgumentParser(description="Create a menubar app with custom text")
+    parser.add_argument("text", type=str, help="Text to display in the menubar")
+    args = parser.parse_args()
+
+    CustomTextApp(args.text).run()
